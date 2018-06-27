698198edeab8ba089746c1e764dd0acdd7460e10
Alberto Fustinoni <alberto@aftnet.net>
2018-06-27 22:06:12 +0900

    Logic

--- video/direct3d/libretro_test_d3d/libretro.cpp
+++ video/direct3d/libretro_test_d3d/libretro.cpp
@@ -5,48 +5,15 @@
 #include <math.h>
 
 #include "libretro_d3d.h"
-#include <d3d11_2.h>
-#include <d2d1_2.h>
-
-#include <DirectXMath.h>
-
-inline void ThrowIfFailed(HRESULT hr)
-{
-    if (FAILED(hr))
-    {
-        throw hr;
-    }
-}
-
-struct ModelViewProjectionConstantBuffer
-{
-    DirectX::XMFLOAT4X4 model;
-    DirectX::XMFLOAT4X4 view;
-    DirectX::XMFLOAT4X4 projection;
-};
-
-	// Used to send per-vertex data to the vertex shader.
-struct VertexPositionColor
-{
-    DirectX::XMFLOAT3 pos;
-    DirectX::XMFLOAT3 color;
-};
+#include "Renderer.h"
 
 static struct retro_hw_render_callback hw_render;
+static const struct retro_hw_render_interface_d3d11 *d3d11;
 
 const unsigned int fb_width = 800;
 const unsigned int fb_height = 600;
 
-ID3D11Device* d3ddevice;
-ID3D11DeviceContext* d3dcontext;
-
-ID3D11VertexShader* vertexShader;
-ID3D11PixelShader* pixelShader;
-ID3D11InputLayout* inputLayout;
-ID3D11Buffer* vertexBuffer;
-ID3D11Buffer* indexBuffer;
-ID3D11Buffer* constantBuffer;
-unsigned int indexCount;
+Renderer D3DRenderer;
 
 void retro_init(void)
 {}
@@ -56,39 +23,39 @@ void retro_deinit(void)
 
 unsigned retro_api_version(void)
 {
-   return RETRO_API_VERSION;
+	return RETRO_API_VERSION;
 }
 
 void retro_set_controller_port_device(unsigned port, unsigned device)
 {
-   (void)port;
-   (void)device;
+	(void)port;
+	(void)device;
 }
 
 void retro_get_system_info(struct retro_system_info *info)
 {
-   memset(info, 0, sizeof(*info));
-   info->library_name     = "TestCore D3D11";
-   info->library_version  = "v1";
-   info->need_fullpath    = false;
-   info->valid_extensions = NULL; // Anything is fine, we don't care.
+	memset(info, 0, sizeof(*info));
+	info->library_name = "TestCore D3D11";
+	info->library_version = "v1";
+	info->need_fullpath = false;
+	info->valid_extensions = NULL; // Anything is fine, we don't care.
 }
 
 void retro_get_system_av_info(struct retro_system_av_info *info)
 {
-    static struct retro_system_timing coreTimings;
-    coreTimings.fps = 60.0;
-    coreTimings.sample_rate = 24000;
+	static struct retro_system_timing coreTimings;
+	coreTimings.fps = 60.0;
+	coreTimings.sample_rate = 24000;
 
-    static struct retro_game_geometry coreGrometry;
-    coreGrometry.base_width   = fb_width;
-    coreGrometry.base_height  = fb_height;
-    coreGrometry.max_width    = fb_width;
-    coreGrometry.max_height   = fb_height;
-    coreGrometry.aspect_ratio = fb_width / fb_height;
+	static struct retro_game_geometry coreGrometry;
+	coreGrometry.base_width = fb_width;
+	coreGrometry.base_height = fb_height;
+	coreGrometry.max_width = fb_width;
+	coreGrometry.max_height = fb_height;
+	coreGrometry.aspect_ratio = fb_width / fb_height;
 
-    info->timing = coreTimings;
-    info->geometry = coreGrometry;
+	info->timing = coreTimings;
+	info->geometry = coreGrometry;
 }
 
 static retro_video_refresh_t video_cb;
@@ -100,172 +67,133 @@ static retro_input_state_t input_state_cb;
 
 void retro_set_environment(retro_environment_t cb)
 {
-   environ_cb = cb;
+	environ_cb = cb;
 
-   struct retro_variable variables[] = {
-      { NULL, NULL },
-   };
+	struct retro_variable variables[] = {
+		{ NULL, NULL },
+	};
 
-   bool no_rom = true;
-   cb(RETRO_ENVIRONMENT_SET_SUPPORT_NO_GAME, &no_rom);
-   cb(RETRO_ENVIRONMENT_SET_VARIABLES, variables);
+	bool no_rom = true;
+	cb(RETRO_ENVIRONMENT_SET_SUPPORT_NO_GAME, &no_rom);
+	cb(RETRO_ENVIRONMENT_SET_VARIABLES, variables);
 }
 
 void retro_set_audio_sample(retro_audio_sample_t cb)
 {
-   audio_cb = cb;
+	audio_cb = cb;
 }
 
 void retro_set_audio_sample_batch(retro_audio_sample_batch_t cb)
 {
-   audio_batch_cb = cb;
+	audio_batch_cb = cb;
 }
 
 void retro_set_input_poll(retro_input_poll_t cb)
 {
-   input_poll_cb = cb;
+	input_poll_cb = cb;
 }
 
 void retro_set_input_state(retro_input_state_t cb)
 {
-   input_state_cb = cb;
+	input_state_cb = cb;
 }
 
 void retro_set_video_refresh(retro_video_refresh_t cb)
 {
-   video_cb = cb;
+	video_cb = cb;
 }
 
 static void update_variables(void)
 {
-   static struct retro_variable var;
-   var.key = "testgl_resolution";
+	static struct retro_variable var;
+	var.key = "testgl_resolution";
 
-   if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
-   {
-   }
+	if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
+	{
+	}
 }
 
 static unsigned frame_count;
 
 void retro_run(void)
 {
-   bool updated = false;
-   if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE_UPDATE, &updated) && updated)
-      update_variables();
+	bool updated = false;
+	if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE_UPDATE, &updated) && updated)
+		update_variables();
 
-   input_poll_cb();
+	input_poll_cb();
 
-   if (input_state_cb(0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_UP))
-   {
-   }
+	if (input_state_cb(0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_UP))
+	{
+	}
 
-   //Render code here
+	//Render code here
 
-   frame_count++;
+	frame_count++;
 
-   video_cb(RETRO_HW_FRAME_BUFFER_VALID, fb_width, fb_height, 0);
+	video_cb(RETRO_HW_FRAME_BUFFER_VALID, fb_width, fb_height, 0);
 }
 
 static void context_reset(void)
 {
+	if (!environ_cb(RETRO_ENVIRONMENT_GET_HW_RENDER_INTERFACE, (void**)&d3d11) || !d3d11)
+	{
+		//Failed to get HW rendering interface
+		return;
+	}
 
-   //Recreate d3d resources here
-   static const VertexPositionColor cubeVertices [] =
-        {
-            { DirectX::XMFLOAT3(-0.5f, -0.5f, -0.5f), DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f) },
-            { DirectX::XMFLOAT3(-0.5f, -0.5f, 0.5f), DirectX::XMFLOAT3(0.0f, 0.0f, 1.0f) },
-            { DirectX::XMFLOAT3(-0.5f, 0.5f, -0.5f), DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f) },
-            { DirectX::XMFLOAT3(-0.5f, 0.5f, 0.5f), DirectX::XMFLOAT3(0.0f, 1.0f, 1.0f) },
-            { DirectX::XMFLOAT3(0.5f, -0.5f, -0.5f), DirectX::XMFLOAT3(1.0f, 0.0f, 0.0f) },
-            { DirectX::XMFLOAT3(0.5f, -0.5f, 0.5f), DirectX::XMFLOAT3(1.0f, 0.0f, 1.0f) },
-            { DirectX::XMFLOAT3(0.5f, 0.5f, -0.5f), DirectX::XMFLOAT3(1.0f, 1.0f, 0.0f) },
-            { DirectX::XMFLOAT3(0.5f, 0.5f, 0.5f), DirectX::XMFLOAT3(1.0f, 1.0f, 1.0f) },
-        };
+	if (d3d11->interface_version != RETRO_HW_RENDER_INTERFACE_D3D11_VERSION)
+	{
+		//Wrong rendering interface version
+		d3d11 = NULL;
+		return;
+	}
 
-        D3D11_SUBRESOURCE_DATA vertexBufferData = { 0 };
-        vertexBufferData.pSysMem = cubeVertices;
-        vertexBufferData.SysMemPitch = 0;
-        vertexBufferData.SysMemSlicePitch = 0;
-        CD3D11_BUFFER_DESC vertexBufferDesc(sizeof(cubeVertices), D3D11_BIND_VERTEX_BUFFER);
-        ThrowIfFailed(d3ddevice->CreateBuffer(&vertexBufferDesc, &vertexBufferData, &vertexBuffer));
-
-        // Load mesh indices. Each triple of indices represents
-        // a triangle to be rendered on the screen.
-        // For example, 0,2,1 means that the vertices with indexes
-        // 0, 2 and 1 from the vertex buffer compose the 
-        // first triangle of this mesh.
-        static const unsigned short cubeIndices [] =
-        {
-            0, 2, 1, // -x
-            1, 2, 3,
-
-            4, 5, 6, // +x
-            5, 7, 6,
-
-            0, 1, 5, // -y
-            0, 5, 4,
-
-            2, 6, 7, // +y
-            2, 7, 3,
-
-            0, 4, 6, // -z
-            0, 6, 2,
-
-            1, 3, 7, // +z
-            1, 7, 5,
-        };
-
-        indexCount = ARRAYSIZE(cubeIndices);
-
-        D3D11_SUBRESOURCE_DATA indexBufferData = { 0 };
-        indexBufferData.pSysMem = cubeIndices;
-        indexBufferData.SysMemPitch = 0;
-        indexBufferData.SysMemSlicePitch = 0;
-        CD3D11_BUFFER_DESC indexBufferDesc(sizeof(cubeIndices), D3D11_BIND_INDEX_BUFFER);
-        ThrowIfFailed(d3ddevice->CreateBuffer(&indexBufferDesc, &indexBufferData, &indexBuffer));
+	//Recreate d3d resources here
+	D3DRenderer.Init();
 }
 
 static void context_destroy(void)
 {
-   //Release d3d stuff here
+	D3DRenderer.Deinit();
 }
 
 static bool retro_init_hw_context(void)
 {
-   hw_render.context_type = RETRO_HW_CONTEXT_OPENGL;
-   hw_render.context_reset = context_reset;
-   hw_render.context_destroy = context_destroy;
-   hw_render.depth = true;
-   hw_render.stencil = true;
-   hw_render.bottom_left_origin = true;
+	hw_render.context_type = RETRO_HW_CONTEXT_DIRECT3D;
+	hw_render.version_major = 11;
+	hw_render.version_minor = 0;
+	hw_render.context_reset = context_reset;
+	hw_render.context_destroy = context_destroy;
+	hw_render.depth = true;
+	hw_render.stencil = true;
+	hw_render.bottom_left_origin = false;
 
-   if (!environ_cb(RETRO_ENVIRONMENT_SET_HW_RENDER, &hw_render))
-      return false;
+	if (!environ_cb(RETRO_ENVIRONMENT_SET_HW_RENDER, &hw_render))
+		return false;
 
-   return true;
+	return true;
 }
 
 bool retro_load_game(const struct retro_game_info *info)
 {
-   update_variables();
+	update_variables();
 
-   enum retro_pixel_format fmt = RETRO_PIXEL_FORMAT_XRGB8888;
-   if (!environ_cb(RETRO_ENVIRONMENT_SET_PIXEL_FORMAT, &fmt))
-   {
-      fprintf(stderr, "XRGB8888 is not supported.\n");
-      return false;
-   }
+	enum retro_pixel_format fmt = RETRO_PIXEL_FORMAT_XRGB8888;
+	if (!environ_cb(RETRO_ENVIRONMENT_SET_PIXEL_FORMAT, &fmt))
+	{
+		//XRGB8888 is not supported
+		return false;
+	}
 
-   if (!retro_init_hw_context())
-   {
-      fprintf(stderr, "HW Context could not be initialized, exiting...\n");
-      return false;
-   }
+	if (!retro_init_hw_context())
+	{
+		//HW Context could not be initialized, exiting...
+		return false;
+	}
 
-   fprintf(stderr, "Loaded game!\n");
-   (void)info;
-   return true;
+	(void)info;
+	return true;
 }
 
 void retro_unload_game(void)
@@ -273,46 +201,46 @@ void retro_unload_game(void)
 
 unsigned retro_get_region(void)
 {
-   return RETRO_REGION_NTSC;
+	return RETRO_REGION_NTSC;
 }
 
 bool retro_load_game_special(unsigned type, const struct retro_game_info *info, size_t num)
 {
-   (void)type;
-   (void)info;
-   (void)num;
-   return false;
+	(void)type;
+	(void)info;
+	(void)num;
+	return false;
 }
 
 size_t retro_serialize_size(void)
 {
-   return 0;
+	return 0;
 }
 
 bool retro_serialize(void *data, size_t size)
 {
-   (void)data;
-   (void)size;
-   return false;
+	(void)data;
+	(void)size;
+	return false;
 }
 
 bool retro_unserialize(const void *data, size_t size)
 {
-   (void)data;
-   (void)size;
-   return false;
+	(void)data;
+	(void)size;
+	return false;
 }
 
 void *retro_get_memory_data(unsigned id)
 {
-   (void)id;
-   return NULL;
+	(void)id;
+	return NULL;
 }
 
 size_t retro_get_memory_size(unsigned id)
 {
-   (void)id;
-   return 0;
+	(void)id;
+	return 0;
 }
 
 void retro_reset(void)
@@ -323,8 +251,8 @@ void retro_cheat_reset(void)
 
 void retro_cheat_set(unsigned index, bool enabled, const char *code)
 {
-   (void)index;
-   (void)enabled;
-   (void)code;
+	(void)index;
+	(void)enabled;
+	(void)code;
 }
 
--- video/direct3d/libretro_test_d3d/renderer.h
+++ video/direct3d/libretro_test_d3d/renderer.h
@@ -1,53 +1,19 @@
-#ifndef _RENDERER_H_
-#define _RENDERER_H_
+#pragma once
 
 #include "libretro_d3d.h"
-#include <DirectXMath.h>
-#include <windows.h>
 
-namespace LRTest
+
+class Renderer
 {
-	struct ModelViewProjectionConstantBuffer
-	{
-		DirectX::XMFLOAT4X4 model;
-		DirectX::XMFLOAT4X4 view;
-		DirectX::XMFLOAT4X4 projection;
-	};
+public:
+	void Init();
+	void Deinit();
+	void RenderFrame();
 
-		// Used to send per-vertex data to the vertex shader.
-	struct VertexPositionColor
-	{
-		DirectX::XMFLOAT3 pos;
-		DirectX::XMFLOAT3 color;
-	};
+private:
+	ID3D11Device * device;
+	ID3D11DeviceContext *context;
+	D3D_FEATURE_LEVEL featureLevel;
+	pD3DCompile D3DCompile;
+};
 
-	class Renderer
-	{
-		private:
-			bool m_ready;
-			pD3DCompile m_shaderCompile;
-
-			ID3D11Device* m_d3ddevice;
-			ID3D11DeviceContext* m_d3dcontext;
-			
-			ID3D11RenderTargetView* m_renderTargetView;
-        	ID3D11DepthStencilView* m_depthStencilView;
-        	ID3D11VertexShader* m_vertexShader;
-        	ID3D11PixelShader* m_pixelShader;
-        	ID3D11InputLayout* m_inputLayout;
-        	ID3D11Buffer* m_vertexBuffer;
-        	ID3D11Buffer* m_indexBuffer;
-        	ID3D11Buffer* m_constantBuffer;
-
-        	ModelViewProjectionConstantBuffer m_constantBufferData;
-
-        	unsigned int m_indexCount;
-
-		public:
-			void CreateResources();
-			void Render();
-			void DestroyResources();
-	};
-}
-
-#endif
