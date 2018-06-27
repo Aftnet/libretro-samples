89d3b04969e3b4461cf526026636821fcb59d045
Alberto Fustinoni <claudiuslollarius@gmail.com>
2018-06-27 14:50:06 +0900

    Logic

--- video/direct3d/libretro_test_d3d/libretro.c
+++ video/direct3d/libretro_test_d3d/libretro.c
@@ -5,11 +5,37 @@
 #include <math.h>
 
 #include "libretro_d3d.h"
+#include <d3d11_2.h>
+#include <d2d1_2.h>
+
+#include <DirectXMath.h>
+
+struct ModelViewProjectionConstantBuffer
+{
+    DirectX::XMFLOAT4X4 model;
+    DirectX::XMFLOAT4X4 view;
+    DirectX::XMFLOAT4X4 projection;
+};
+
+	// Used to send per-vertex data to the vertex shader.
+struct VertexPositionColor
+{
+    DirectX::XMFLOAT3 pos;
+    DirectX::XMFLOAT3 color;
+};
 
 static struct retro_hw_render_callback hw_render;
 
 const unsigned int fb_width = 800;
-const unsigned int fb_height = 800;
+const unsigned int fb_height = 600;
+
+ID3D11VertexShader* vertexShader;
+ID3D11PixelShader* pixelShader;
+ID3D11InputLayout* inputLayout;
+ID3D11Buffer* vertexBuffer;
+ID3D11Buffer* indexBuffer;
+ID3D11Buffer* constantBuffer;
+unsigned int indexCount;
 
 void retro_init(void)
 {}
@@ -133,6 +159,59 @@ void retro_run(void)
 static void context_reset(void)
 {
    //Recreate d3d resources here
+   static const VertexPositionColor cubeVertices [] =
+        {
+            { XMFLOAT3(-0.5f, -0.5f, -0.5f), XMFLOAT3(0.0f, 0.0f, 0.0f) },
+            { XMFLOAT3(-0.5f, -0.5f, 0.5f), XMFLOAT3(0.0f, 0.0f, 1.0f) },
+            { XMFLOAT3(-0.5f, 0.5f, -0.5f), XMFLOAT3(0.0f, 1.0f, 0.0f) },
+            { XMFLOAT3(-0.5f, 0.5f, 0.5f), XMFLOAT3(0.0f, 1.0f, 1.0f) },
+            { XMFLOAT3(0.5f, -0.5f, -0.5f), XMFLOAT3(1.0f, 0.0f, 0.0f) },
+            { XMFLOAT3(0.5f, -0.5f, 0.5f), XMFLOAT3(1.0f, 0.0f, 1.0f) },
+            { XMFLOAT3(0.5f, 0.5f, -0.5f), XMFLOAT3(1.0f, 1.0f, 0.0f) },
+            { XMFLOAT3(0.5f, 0.5f, 0.5f), XMFLOAT3(1.0f, 1.0f, 1.0f) },
+        };
+
+        D3D11_SUBRESOURCE_DATA vertexBufferData = { 0 };
+        vertexBufferData.pSysMem = cubeVertices;
+        vertexBufferData.SysMemPitch = 0;
+        vertexBufferData.SysMemSlicePitch = 0;
+        CD3D11_BUFFER_DESC vertexBufferDesc(sizeof(cubeVertices), D3D11_BIND_VERTEX_BUFFER);
+        ThrowIfFailed(m_d3dDevice->CreateBuffer(&vertexBufferDesc, &vertexBufferData, &m_vertexBuffer));
+
+        // Load mesh indices. Each triple of indices represents
+        // a triangle to be rendered on the screen.
+        // For example, 0,2,1 means that the vertices with indexes
+        // 0, 2 and 1 from the vertex buffer compose the 
+        // first triangle of this mesh.
+        static const unsigned short cubeIndices [] =
+        {
+            0, 2, 1, // -x
+            1, 2, 3,
+
+            4, 5, 6, // +x
+            5, 7, 6,
+
+            0, 1, 5, // -y
+            0, 5, 4,
+
+            2, 6, 7, // +y
+            2, 7, 3,
+
+            0, 4, 6, // -z
+            0, 6, 2,
+
+            1, 3, 7, // +z
+            1, 7, 5,
+        };
+
+        m_indexCount = ARRAYSIZE(cubeIndices);
+
+        D3D11_SUBRESOURCE_DATA indexBufferData = { 0 };
+        indexBufferData.pSysMem = cubeIndices;
+        indexBufferData.SysMemPitch = 0;
+        indexBufferData.SysMemSlicePitch = 0;
+        CD3D11_BUFFER_DESC indexBufferDesc(sizeof(cubeIndices), D3D11_BIND_INDEX_BUFFER);
+        ThrowIfFailed(m_d3dDevice->CreateBuffer(&indexBufferDesc, &indexBufferData, &m_indexBuffer));
 }
 
 static void context_destroy(void)
